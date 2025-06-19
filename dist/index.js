f1d29a96f71cd0f888e6420190bf010ec503441e
Khan4218 <mirakber4@gmail.com>
2025-06-19 16:13:01 +0530

    Being explicit

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,13 @@
 "use strict";
 const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
+    { id: 1, name: "Margherita", price: 8 },
+    { id: 2, name: "Pepperoni", price: 10 },
+    { id: 3, name: "Hawaiian", price: 10 },
+    { id: 4, name: "Veggie", price: 9 },
 ];
 let cashInRegister = 100;
 let nextOrderId = 1;
-const orderQueue = [];
+const orderHistory = [];
 function addNewPizza(pizzaObj) {
     menu.push(pizzaObj);
 }
@@ -19,11 +19,11 @@ function placeOrder(pizzaName) {
     }
     cashInRegister += selectedPizza.price;
     const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" };
-    orderQueue.push(newOrder);
+    orderHistory.push(newOrder);
     return newOrder;
 }
 function completeOrder(orderId) {
-    const order = orderQueue.find(order => order.id === orderId);
+    const order = orderHistory.find(order => order.id === orderId);
     if (!order) {
         console.error(orderId, "Not found");
         return;
@@ -31,11 +31,22 @@ function completeOrder(orderId) {
     order.status = "completed";
     return order;
 }
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
-addNewPizza({ name: "BBQ Chicken", price: 12 });
-addNewPizza({ name: "Spicy Sausage", price: 11 });
+function getPizzaDetail(identifier) {
+    if (typeof identifier === "string") {
+        return menu.find(pizza => pizza.name.toLowerCase === identifier.toLowerCase);
+    }
+    else if (typeof identifier === "number") {
+        return menu.find(pizza => pizza.id === identifier);
+    }
+    else {
+        throw new TypeError("Parameter `identifier` must be either a string or a number");
+    }
+}
+addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 });
+addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 });
 placeOrder("Chicken Bacon Ranch");
 completeOrder(1);
 console.log("Menu:", menu);
 console.log("Cash in register:", cashInRegister);
-console.log("Order queue:", orderQueue);
+console.log("Order queue:", orderHistory);
--- dist/practice.js
+++ dist/practice.js
@@ -11,3 +11,7 @@ let person2 = {
     isStudent: false, // beneift of using type this error shows we dont have isstudent in type person now i am switching  to isStudent instead
 };
 let people = [person1, person2];
+//Literal types
+let myName = "khan"; // with let i can change my value to any datatype
+const myName2 = "Akbar"; // with const i can't and leteral type
+let userRole = "member";
