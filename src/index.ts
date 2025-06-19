84ae1658c63b96f14ddfc695f043f24098af2fb8
Khan4218 <mirakber4@gmail.com>
2025-06-19 08:54:13 +0530

    obligatory type basic lesson

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,58 @@
-const menu = [
-  { name: "Margherita", price: 8 },
-  { name: "Pepperoni", price: 10 },
-  { name: "Hawaiian", price: 10 },
-  { name: "Veggie", price: 9 },
-]
+// const menu = [
+//   { name: "Margherita", price: 8 },
+//   { name: "Pepperoni", price: 10 },
+//   { name: "Hawaiian", price: 10 },
+//   { name: "Veggie", price: 9 },
+// ]
 
 
 
-let cashInRegister = 100
-let nextOrderId = 1
-const orderQueue = []
+// let cashInRegister = 100
+// let nextOrderId = 1
+// const orderQueue = []
 
-function addNewPizza(pizzaObj) {
-  menu.push(pizzaObj)
-}
-
-
-function placeHolder(pizzaName) {
-  const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
-  cashInRegister += selectedPizza.price
-  const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
-  newOrder.push(orderQueue)
-  return newOrder
-
-}
-
-function completeOrder(orderId) {
-  const order = orderQueue.find(order => order.id === orderId)
-  order.status = "completed"
-  return order
-}
+// function addNewPizza(pizzaObj) {
+//   menu.push(pizzaObj)
+// }
 
 
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
-addNewPizza({ name: "BBQ Chicken", cost: 12 })
-addNewPizza({ name: "Spicy Sausage", cost: 11 })
+// function placeHolder(pizzaName) {
+//   const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+//   cashInRegister += selectedPizza.price
+//   const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+//   newOrder.push(orderQueue)
+//   return newOrder
 
-placeOrder("Chicken Bacon Ranch")
-completeOrder("1")
+// }
 
-console.log("Menu:", menu)
-console.log("Cash in register:", cashInRegister)
-console.log("Order queue:", orderQueue)
+// function completeOrder(orderId) {
+//   const order = orderQueue.find(order => order.id === orderId)
+//   order.status = "completed"
+//   return order
+// }
+
+
+// addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
+// addNewPizza({ name: "BBQ Chicken", cost: 12 })
+// addNewPizza({ name: "Spicy Sausage", cost: 11 })
+
+// placeOrder("Chicken Bacon Ranch")
+// completeOrder("1")
+
+// console.log("Menu:", menu)
+// console.log("Cash in register:", cashInRegister)
+// console.log("Order queue:", orderQueue)
+
+
+// Obligatory Type Basic lesson
+
+// let myName = "khan" javascript syntax
+let myName: string = "khan" //TypeScript syntax
+
+// let numberOfWheels = 2 // js syntax
+let numberOfWheels: number = 2 // Ts stntax
+
+// let isStudent = false // js syntax boolean
+let isStudent: boolean = false // Ts syntax
 
 
