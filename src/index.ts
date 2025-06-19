f9b0971095cbe90aeda1cd50c0cf19b0fc5f086b
Khan4218 <mirakber4@gmail.com>
2025-06-19 15:20:46 +0530

    Update order status to use literal type status

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ type Pizza = {
 type Order = {
   id: number
   pizza: Pizza
-  status: string
+  status: "ordered" | "completed"
 
 }
 
@@ -36,7 +36,7 @@ function placeOrder(pizzaName: string) {
     return;
   }
   cashInRegister += selectedPizza.price
-  const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+  const newOrder: Order = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
   orderHistory.push(newOrder)
   return newOrder
 
