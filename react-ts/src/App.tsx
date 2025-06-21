7fe2cf9fb3f239b4b2c74e3b3b305457fe4e85d2
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-06-21 10:46:57 +0530

    create a new state and a handleAdd function

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import InputField from './components/InputField'
+import type { Todo } from './model'
 
 const App: React.FC=() => {
     
    const [todo, setTodo] = useState<string>("")
-   console.log(todo);
+   const [todos, setTodos] = useState<Todo[]>([])
+
+   const handleAdd = (e: React.FormEvent) => {
+      e.preventDefault();
+      if (todo) {
+       setTodos([...todos, {id :Date.now(), todo : todo, isDone : false}])
+       setTodo("");
+      }
+   }
+   console.log(todos);
    
    return (<div className='App'>
       <span className='heading'> Taskify</span>
-      <InputField todo={todo} setTodo ={setTodo}/>
+      <InputField todo={todo} setTodo ={setTodo} handleAdd = {handleAdd}/>
    </div>)
 }
 
