9990e6fa6492d45756830152387d2f7ca85292aa
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-06-21 09:03:37 +0530

    Taskify Project input component and styles to input and button

---
+++ react-ts/src/components/InputField.tsx
@@ -0,0 +1,12 @@
+import "./styles.css"
+const InputField = () => {
+  return (
+    <form className="input">
+        <input type="input" placeholder="Enter a Task" className="input_box"/>
+        <button className="input_submit" type="submit" >Go</button>
+    </form>
+    
+  )
+}
+
+export default InputField
