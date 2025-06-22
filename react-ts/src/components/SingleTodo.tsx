191d21f76b36297f3f1b16862ace88b4b3566012
Khan4218 <mirakber4@gmail.com>
2025-06-22 07:13:31 +0530

    handleDone and Handle Delete using Types

--- react-ts/src/components/SingleTodo.tsx
+++ react-ts/src/components/SingleTodo.tsx
@@ -1,30 +1,44 @@
 import React from 'react'
 import type { Todo } from '../model'
-import {MdModeEdit, MdDelete, MdDone} from 'react-icons/md'
-type Props ={
-    todo :Todo;
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+import { MdModeEdit, MdDelete, MdDone } from 'react-icons/md'
+
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 
 }
-const SingleTodo = ({todo, todos, setTodos}: Props) => {
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+
+
+  const handleDone = (id: number) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)))
+  }
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
   return (
     <form className='todos__single'>
-    <div >
-     <span className="todos__single--text">
-        {todo.todo}
-     </span>
-    </div>
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo} </s>
 
-    <span className="icon">
+      ) : (
+        <span className="todos__single--text">{todo.todo} </span>
+      )}
+
+
+
+      <span className="icon">
         <MdModeEdit />
-    </span>
-    <span className="icon">
+      </span>
+      <span className="icon" onClick={() => handleDelete(todo.id)}>
         <MdDelete />
-    </span>
-    <span className="icon">
+      </span>
+      <span className="icon" onClick={() => handleDone(todo.id)}>
         <MdDone />
-    </span>
+      </span>
     </form>
   )
 }
