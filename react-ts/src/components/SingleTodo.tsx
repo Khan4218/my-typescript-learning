4c96354cb26735d23c13da4138a153bb2910bfbf
Khan4218 <mirakber4@gmail.com>
2025-06-22 08:17:42 +0530

    Handling Edit using Hooks and Types

--- react-ts/src/components/SingleTodo.tsx
+++ react-ts/src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import type { Todo } from '../model'
 import { MdModeEdit, MdDelete, MdDone } from 'react-icons/md'
 
@@ -9,7 +9,8 @@ type Props = {
 
 }
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
-
+  const [edit, setEdit] = useState<boolean>(false)
+  const [editTodo, setEditTodo] = useState<string>(todo.todo)
 
   const handleDone = (id: number) => {
     setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)))
@@ -19,18 +20,51 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   }
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault()
+
+    setTodos(
+      todos.map((todo) => (
+        todo.id === id ? { ...todo, todo: editTodo } : todo
+      ))
+    );
+    setEdit(false);
+  }
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit])
+
+
   return (
-    <form className='todos__single'>
-      {todo.isDone ? (
-        <s className="todos__single--text">{todo.todo} </s>
+    <form className='todos__single' onSubmit={(e) => handleEdit(e, todo.id)}>
+      {
+        edit ? (
+          <input
+            ref={inputRef}
+            value={editTodo}
+            onChange={(e) => setEditTodo(e.target.value)}
+            className='"todos__single--text"' />
+        ) :
+          todo.isDone ? (
+            <s className="todos__single--text">{todo.todo} </s>
 
-      ) : (
-        <span className="todos__single--text">{todo.todo} </span>
-      )}
+          ) : (
+            <span className="todos__single--text">{todo.todo} </span>
+          )
+
+      }
 
 
 
-      <span className="icon">
+      <span className="icon"
+        onClick={() => {
+          if (!edit && !todo.isDone) {
+            setEdit(!edit);
+          }
+        }}>
         <MdModeEdit />
       </span>
       <span className="icon" onClick={() => handleDelete(todo.id)}>
